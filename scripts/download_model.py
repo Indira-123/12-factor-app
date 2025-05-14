34c3deaf4ada1aa1ed20add1a539c08be525ca8d
Indira Sharma <indisharma314@gmail.com>
2025-05-14 18:10:32 +0545

    fix(model): replace private model with public model for accessibility

--- scripts/download_model.py
+++ scripts/download_model.py
@@ -1,7 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 def download_model():
-    model_name = "cardiffnlp/twitter-roberta-base-sentiment"
+    model_name = "distilbert-base-uncased-finetuned-sst-2-english"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
